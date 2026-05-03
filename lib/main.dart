ea9455c1f6ade7241234a888a95588ed83908b82
sanaul-writes-code <165845997+sanaul-writes-code@users.noreply.github.com>
2026-05-03 15:04:36 -0400

    Initialize firebase in flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/quest_screen.dart';
@@ -10,6 +12,11 @@ import 'screens/intro_video_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const StepQuestApp());
 }
 
